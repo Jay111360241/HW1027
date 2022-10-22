8278af24bd26fd1ff2a5296e6e8d45fb81ecb3f8
Jay111360241 <jay0718456@gmail.com>
2022-10-22 20:23:43 +0800

    HW

---
+++ 13/source/Main.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+double Power(double, int);
+
+void main(void)
+{
+	int k;
+	double ans; 
+	printf("計算3.5的k次方?請輸入k=");
+	scanf_s("%d", &k);
+	ans = Power(3.5, k);
+	printf("3.5的%d次方=%f\n", k, ans);
+
+	system("pause");
+}
+
+double Power(double X, int n)
+{
+	int i;
+	double PowerXn = 1;
+	for (i = 1; i <= n; i++)
+		PowerXn = PowerXn * X;
+	return PowerXn;
+
+
+}
